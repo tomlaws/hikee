a3ff92644c47f8af7e06946c5a2ad5ad157fdd40
Tom Law <lawhowang@icloud.com>
2021-06-28 15:20:08 +0800

    add sliding panel

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,8 +13,18 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(child: Text('Home')),
+      body: SlidingUpPanel(
+        parallaxEnabled: true,
+        maxHeight: MediaQuery.of(context).size.height - 160,
+        boxShadow: [],
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(36), topRight: Radius.circular(36)),
+        panel: Center(
+          child: Text('Sliding panel'),
+        ),
+        body: Container(
+          color: Color(0xFF5DB075),
+        ),
       ),
     );
   }
