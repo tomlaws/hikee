3ad60d6c910935b727a9cbdcade36109debeb279
Tom Law <lawhowang@icloud.com>
2021-07-04 22:30:17 +0800

    update route model

--- lib/models/route.dart
+++ lib/models/route.dart
@@ -1,15 +1,34 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class Route {
   final String name;
-  final DateTime updatedAt;
+  final String name_en;
+  final int difficulty;
+  final double duration;
+  final double length;
+  final List<LatLng?> polyline;
+  final DateTime? updatedAt;
 
-  Route(this.name, this.updatedAt);
+  Route(this.name, this.name_en, this.difficulty, this.duration, this.length,
+      this.polyline, this.updatedAt);
 
   Route.fromJson(Map<String, dynamic> json)
       : name = json['name'],
+        name_en = json['name_en'],
+        difficulty = json['difficulty'],
+        duration = json['duration'],
+        length = json['length'],
+        polyline =
+            (json['polyline'] as List).map((e) => LatLng.fromJson(e)).toList(),
         updatedAt = json['updatedAt'];
 
   Map<String, dynamic> toJson() => {
         'name': name,
+        'name_en': name_en,
+        'difficulty': difficulty,
+        'duration': duration,
+        'length': length,
+        'polyline': polyline.map((e) => e?.toJson()).toList(),
         'updatedAt': updatedAt,
       };
 }
