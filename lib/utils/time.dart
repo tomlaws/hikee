dbc9b2e8f68dd113201daff2cf9f986f5ce78170
Tom Law <lawhowang@icloud.com>
2021-07-14 21:58:42 +0800

    fix time utils

--- lib/utils/time.dart
+++ lib/utils/time.dart
@@ -3,6 +3,9 @@ class TimeUtils {
     int remainder = minutes % 60;
     int hour = (minutes / 60).floor();
     if (hour > 0) {
+      if (remainder == 0) {
+        return '${hour}h';
+      }
       return '${hour}h ${remainder}m';
     } else {
       return '${remainder}m';
